5e93995f13e3176491733f936699a48fc792267a
Tomek <tomekdev@users.noreply.github.com>
2017-03-10 22:45:27 +0100

    Front-end app base

---
+++ controllers/static.js
@@ -0,0 +1,12 @@
+var path = require('path')
+var express = require('express')
+var router  = express.Router()
+
+router.use(express.static(__dirname + '/../assets'))
+router.use('/views', express.static(__dirname + '/../views'))
+
+router.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/../templates/app.html'))
+})
+
+module.exports = router
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var ngAnnotate = require('gulp-ng-annotate')
+var sourcemaps = require('gulp-sourcemaps')
+
+gulp.task('js', function () {
+    return gulp.src(['app/module.js', 'app/**/*.js'])
+        .pipe(ngAnnotate())
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('assets'))
+})
+
+gulp.task('watch', ['js'], function () {
+    gulp.watch('app/**/*.js', ['js'])
+})
